9e598074950d5874c8cfdef0f8024d75a2c7e603
effectivemadness <madcat@khu.ac.kr>
2021-11-27 12:29:55 +0900

    feat: added stylegan2 model

---
+++ DiffAugment-stylegan2-pytorch/DiffAugment_pytorch.py
@@ -0,0 +1,151 @@
+# Differentiable Augmentation for Data-Efficient GAN Training
+# Shengyu Zhao, Zhijian Liu, Ji Lin, Jun-Yan Zhu, and Song Han
+# https://arxiv.org/pdf/2006.10738
+
+import torch
+import torch.nn.functional as F
+import numpy as np
+import torchvision
+
+def DiffAugment(x, policy='', channels_first=True):
+    if policy:
+        if not channels_first:
+            x = x.permute(0, 3, 1, 2)
+        for p in policy.split(','):
+            for f in AUGMENT_FNS[p]:
+                x = f(x)
+        if not channels_first:
+            x = x.permute(0, 2, 3, 1)
+        x = x.contiguous()
+    return x
+
+
+def rand_brightness(x):
+    x = x + (torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device) - 0.5)
+    return x
+
+
+def rand_saturation(x):
+    x_mean = x.mean(dim=1, keepdim=True)
+    x = (x - x_mean) * (torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device) * 2) + x_mean
+    return x
+
+
+def rand_contrast(x):
+    x_mean = x.mean(dim=[1, 2, 3], keepdim=True)
+    x = (x - x_mean) * (torch.rand(x.size(0), 1, 1, 1, dtype=x.dtype, device=x.device) + 0.5) + x_mean
+    return x
+
+
+def rand_translation(x, ratio=0.125):
+    shift_x, shift_y = int(x.size(2) * ratio + 0.5), int(x.size(3) * ratio + 0.5)
+    translation_x = torch.randint(-shift_x, shift_x + 1, size=[x.size(0), 1, 1], device=x.device)
+    translation_y = torch.randint(-shift_y, shift_y + 1, size=[x.size(0), 1, 1], device=x.device)
+    grid_batch, grid_x, grid_y = torch.meshgrid(
+        torch.arange(x.size(0), dtype=torch.long, device=x.device),
+        torch.arange(x.size(2), dtype=torch.long, device=x.device),
+        torch.arange(x.size(3), dtype=torch.long, device=x.device),
+    )
+    grid_x = torch.clamp(grid_x + translation_x + 1, 0, x.size(2) + 1)
+    grid_y = torch.clamp(grid_y + translation_y + 1, 0, x.size(3) + 1)
+    x_pad = F.pad(x, [1, 1, 1, 1, 0, 0, 0, 0])
+    x = x_pad.permute(0, 2, 3, 1).contiguous()[grid_batch, grid_x, grid_y].permute(0, 3, 1, 2).contiguous()
+    return x
+
+def grid_sample(image, optical):
+    N, C, IH, IW = image.shape
+    _, H, W, _ = optical.shape
+
+    ix = optical[..., 0]
+    iy = optical[..., 1]
+
+    ix = ((ix + 1) / 2) * (IW-1);
+    iy = ((iy + 1) / 2) * (IH-1);
+    with torch.no_grad():
+        ix_nw = torch.floor(ix);
+        iy_nw = torch.floor(iy);
+        ix_ne = ix_nw + 1;
+        iy_ne = iy_nw;
+        ix_sw = ix_nw;
+        iy_sw = iy_nw + 1;
+        ix_se = ix_nw + 1;
+        iy_se = iy_nw + 1;
+
+    nw = (ix_se - ix)    * (iy_se - iy)
+    ne = (ix    - ix_sw) * (iy_sw - iy)
+    sw = (ix_ne - ix)    * (iy    - iy_ne)
+    se = (ix    - ix_nw) * (iy    - iy_nw)
+    
+    with torch.no_grad():
+        torch.clamp(ix_nw, 0, IW-1, out=ix_nw)
+        torch.clamp(iy_nw, 0, IH-1, out=iy_nw)
+
+        torch.clamp(ix_ne, 0, IW-1, out=ix_ne)
+        torch.clamp(iy_ne, 0, IH-1, out=iy_ne)
+ 
+        torch.clamp(ix_sw, 0, IW-1, out=ix_sw)
+        torch.clamp(iy_sw, 0, IH-1, out=iy_sw)
+ 
+        torch.clamp(ix_se, 0, IW-1, out=ix_se)
+        torch.clamp(iy_se, 0, IH-1, out=iy_se)
+
+    image = image.view(N, C, IH * IW)
+
+
+    nw_val = torch.gather(image, 2, (iy_nw * IW + ix_nw).long().view(N, 1, H * W).repeat(1, C, 1))
+    ne_val = torch.gather(image, 2, (iy_ne * IW + ix_ne).long().view(N, 1, H * W).repeat(1, C, 1))
+    sw_val = torch.gather(image, 2, (iy_sw * IW + ix_sw).long().view(N, 1, H * W).repeat(1, C, 1))
+    se_val = torch.gather(image, 2, (iy_se * IW + ix_se).long().view(N, 1, H * W).repeat(1, C, 1))
+
+    out_val = (nw_val.view(N, C, H, W) * nw.view(N, 1, H, W) + 
+               ne_val.view(N, C, H, W) * ne.view(N, 1, H, W) +
+               sw_val.view(N, C, H, W) * sw.view(N, 1, H, W) +
+               se_val.view(N, C, H, W) * se.view(N, 1, H, W))
+
+    return out_val
+
+def rand_cutout(x, ratio=0.5):
+    cutout_size = int(x.size(2) * ratio + 0.5), int(x.size(3) * ratio + 0.5)
+    offset_x = torch.randint(0, x.size(2) + (1 - cutout_size[0] % 2), size=[x.size(0), 1, 1], device=x.device)
+    offset_y = torch.randint(0, x.size(3) + (1 - cutout_size[1] % 2), size=[x.size(0), 1, 1], device=x.device)
+    grid_batch, grid_x, grid_y = torch.meshgrid(
+        torch.arange(x.size(0), dtype=torch.long, device=x.device),
+        torch.arange(cutout_size[0], dtype=torch.long, device=x.device),
+        torch.arange(cutout_size[1], dtype=torch.long, device=x.device),
+    )
+    grid_x = torch.clamp(grid_x + offset_x - cutout_size[0] // 2, min=0, max=x.size(2) - 1)
+    grid_y = torch.clamp(grid_y + offset_y - cutout_size[1] // 2, min=0, max=x.size(3) - 1)
+    mask = torch.ones(x.size(0), x.size(2), x.size(3), dtype=x.dtype, device=x.device)
+    mask[grid_batch, grid_x, grid_y] = 0
+    x = x * mask.unsqueeze(1)
+    return x
+
+def get_rot_mat(theta):
+    # theta = torch.tensor(theta, device=theta.device) # 수정사항.
+    return torch.tensor([[torch.cos(theta), -torch.sin(theta), 0],
+                         [torch.sin(theta), torch.cos(theta), 0]], device=theta.device)
+
+
+def rand_rotation(x, max_theta=20):
+    # print(x.size())
+    # before_img = x.cpu()[0].permute(1, 2, 0).detach().numpy()
+    # print(before_img.shape)
+    # cv2.imwrite('before.jpg', before_img)
+    # torchvision.utils.save_image(x.cpu()[0], 'before.png',normalize=True)
+    theta = (torch.rand(1, device=x.device)[0] * 2 - 1) * np.pi * (max_theta / 180)
+    # print(theta)
+    rot_mat = get_rot_mat(theta)[None, ...].type(torch.cuda.FloatTensor).repeat(x.shape[0],1,1)
+    grid = F.affine_grid(rot_mat, x.size(),align_corners=True).type(torch.cuda.FloatTensor) # align_corners default = false. modded.
+    x = grid_sample(x, grid) # align_corners default = false. modded.
+    # print("rotation")
+    # after_img = x.cpu()[0].permute(1, 2, 0).numpy()
+    # cv2.imwrite('after.png', after_img)
+    # torchvision.utils.save_image(x.cpu()[0], 'after.png',normalize=True)
+    return x
+
+AUGMENT_FNS = {
+    'color': [rand_brightness, rand_saturation, rand_contrast],
+    'translation': [rand_translation],
+    'cutout': [rand_cutout],
+    'rotation': [rand_rotation]
+}
